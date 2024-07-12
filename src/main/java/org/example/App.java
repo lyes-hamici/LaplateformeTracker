9ed12f6d35acec90846697b67468bca808977493
oroitz-lago-ramos <oroitz.lago-ramos@laplateforme.io>
2024-07-12 16:13:33 +0200

    <create>[FileManager]:<created import export management class>

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -12,10 +12,10 @@ public class App {
     public static void main(String[] args) throws Exception {
         Db db = new Db();
         UserRepo userRepo = new UserRepo(db);
-        boolean isValid = userRepo.isValidUser("root", "root");
-        System.out.println(isValid);
-        db.endConnection();
+
+
 
         // Menu.menu();
+        db.endConnection();
     }
 }
